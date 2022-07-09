49ffa5c7bd5b0d925985a792155163a2ad67fb53
Jeffrey den Drijver <jeffreydendrijver@gmail.com>
2022-07-09 22:09:10 +0200

    Add actual functionality to thrust_processor

--- minisub_control/twist_thrust_processor.py
+++ minisub_control/twist_thrust_processor.py
@@ -1,15 +1,24 @@
+import enum
+import numpy, enum
 import rclpy
 from rclpy.node import Node
 
 from geometry_msgs.msg import Twist
-from std_msgs.msg import Int64MultiArray
+from std_msgs.msg import Float32MultiArray
 
-class ThrustControl(Node):
+class ThrusterType(enum.Enum):
+    Horizontal = 1
+    Vertical = 2
+
+class ThrusterConfig():
+    type = ThrusterType.Horizontal
+
+class TwistThrustProcessor(Node):
     def __init__(self):
-        super().__init__('ms_thr_ctl')
+        super().__init__('twist_thrust_processor')
 
         self.thr_pwr_publisher_ = self.create_publisher(
-            Int64MultiArray, 
+            Float32MultiArray, 
             'thrusters_power', 
             10
         )
@@ -20,33 +29,49 @@ class ThrustControl(Node):
             self.twist_subscription_cb,
             10
         )
-        self.twist_subscription  # prevent unused variable warning
 
+        # FIXME: Use parameter, allow to be configured per thruster
+        self.inverse_thrust = True
+        
     def twist_subscription_cb(self, msg):
         self.get_logger().info('I heard: "%s"' % msg)
-        
-        self.thr_pwr_publish(0.5, 0.5)
 
+        speed = numpy.clamp(msg.linear.x, -1, 1)
+        rotation = msg.angular.z
 
-    def thr_pwr_publish(self, x_left, x_right):
-        msg = Int64MultiArray
-        msg.data[0] = x_left
-        msg.data[1] = x_right
+        if(self.inverse_thrust):
+            speed = -speed
         
+        left = speed
+        right = speed
+
+        if(rotation != 0):
+            if(speed != 0):
+                if(rotation > 0):
+                    left = speed
+                    right = speed * rotation
+                elif(rotation < 0):
+                    left = speed * -rotation
+                    right = speed
+            else:
+                # These come out way to high
+                left = -rotation
+                right = rotation
+        
+        msg = Float32MultiArray()
+        msg.data = [left, right]
+
         self.thr_pwr_publisher_.publish(msg)
         
         self.get_logger().info('Publishing: "%s"' % msg.data[0])
-        self.i += 1
-
+        
 def main(args=None):
     rclpy.init(args=args)
 
-    thrust_control_node = ThrustControl()
+    thrust_control_node = TwistThrustProcessor()
 
     rclpy.spin(thrust_control_node)
 
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
     thrust_control_node.destroy_node()
+
     rclpy.shutdown()
